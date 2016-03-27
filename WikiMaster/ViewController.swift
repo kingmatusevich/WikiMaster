0337e5200b0f7f8f6df3b4801bcbd64bf978ab4a
Javier Matusevich <devmatusevich@gmail.com>
2016-03-27 16:22:22 -0300

    Initial Commit

---
+++ WikiMaster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WikiMaster
+//
+//  Created by Javier Matusevich on 27/3/16.
+//  Copyright © 2016 Javier Matusevich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
